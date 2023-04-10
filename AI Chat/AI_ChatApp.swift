8fa2d561043d65e96ba0e61c949fe5a1e6f43906
Johan Wigmo <johanwigmo@gmail.com>
2023-04-10 21:59:12 +0200

    Initial Commit

---
+++ AI Chat/AI_ChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  AI_ChatApp.swift
+//  AI Chat
+//
+//  Created by Johan Wigmo on 2023-04-10.
+//
+
+import SwiftUI
+
+@main
+struct AI_ChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
